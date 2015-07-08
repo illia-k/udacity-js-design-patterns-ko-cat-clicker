a1023a7ba601132450208e6bdcde10fca5990e8d
unknown <illia.kyrylenko.gmail.com>
2015-07-08 19:07:33 +0300

    basic fanctionality

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/// <reference path="lib/knockout-3.2.0.js" />
+
+
+var ViewModel = function() {
+	this.name = ko.observable("Kote");
+	this.clickNum = ko.observable(0);
+	this.catPicUrl = ko.observable("img/1413379559_412a540d29_z.jpg");
+	
+	this.changeClickNum = function () {
+		this.clickNum(this.clickNum() + 1);
+	}
+	this.title = ko.computed(function(){
+		if(this.clickNum() >= 20)
+			return "veteran";
+			
+		if(this.clickNum() >= 15)
+			return "mature";
+		
+		if(this.clickNum() >= 10)
+			return "pre-mature";
+		
+		if(this.clickNum() >= 5)
+			return "teen";	
+			
+		return "newborn";
+	}, this);
+	
+};
+
+ko.applyBindings(new ViewModel());
