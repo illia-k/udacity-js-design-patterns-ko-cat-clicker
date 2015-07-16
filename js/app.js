42a065b51a749115e2b6f47b81e0afeb08228f83
illia-k <illia.kyrylenko@gmail.com>
2015-07-17 00:38:45 +0300

    Add cat selector. Add cats data.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
 /// <reference path="lib/knockout-3.2.0.js" />
-var Cat = function() {
-	this.name = ko.observable("Kote");
-	this.clickNum = ko.observable(0);
-	this.catPicUrl = ko.observable("img/1413379559_412a540d29_z.jpg");
-	this.nickNames = ko.observableArray([
-		{name: "Kote"},
-		{name: "Kotee"},
-		{name: "Koteshechka"},
-		{name: "Kot'e"}		
-	]);	
+var initialCats = [
+	{id:0,
+	name: "Kote",
+	clickNum: 0, 
+	catPicUrl:"img/1413379559_412a540d29_z.jpg",
+	nickNames: [{name: "Kote"},	{name: "Kotee"}, {name: "Koteshechka"},	{name: "Kot'e"}]},
+	{id:1,
+	name: "Kote1",
+	clickNum: 0, 
+	catPicUrl:"img/22252709_010df3379e_z.jpg",
+	nickNames: [{name: "the first"}]},
+	{id:2,
+	name: "Kote2",
+	clickNum: 0, 
+	catPicUrl:"img/4154543904_6e2428c421_z.jpg",
+	nickNames: [{name: "the second"}]},
+	{id:3,
+	name: "Kote3",
+	clickNum: 0, 
+	catPicUrl:"img/9648464288_2516b35537_z.jpg",
+	nickNames: [{name: "the third"}]}
+];
+
+var Cat = function(data) {
+	this.id = ko.observable(data.id);
+	this.name = ko.observable(data.name);
+	this.clickNum = ko.observable(data.clickNum);
+	this.catPicUrl = ko.observable(data.catPicUrl);
+	this.nickNames = ko.observableArray(data.nickNames);	
 	this.title = ko.computed(function(){
 		if(this.clickNum() >= 20)
 			return "veteran";
@@ -28,11 +47,22 @@ var Cat = function() {
 
 
 var ViewModel = function() {
-	var self = this;	
-	this.currentCat = ko.observable(new Cat());
+	var self = this;
+	this.catList = ko.observableArray([]);
+	
+	initialCats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+	});
+		
+//	this.currentCat = ko.observable(this.catList()[2]);
+	this.currentCat = ko.observable();
+	
 	this.changeClickNum = function () {
-		self.currentCat().clickNum(self.currentCat().clickNum() + 1);
-	};	
+		var currentCatId = self.currentCat().id();
+		self.catList()[currentCatId].clickNum(
+			self.catList()[currentCatId].clickNum() + 1);
+	};
+
 };
 
 ko.applyBindings(new ViewModel());
